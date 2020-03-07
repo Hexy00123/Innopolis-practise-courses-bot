164372b866953a5cc5b095b5c243b4af00feb58b
user <r.izmajlov2018@yandex.ru>
2020-03-07 18:41:21 +0300

    FIXED

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from decouple import config
 from playhouse.db_url import connect
 
 TOKEN = config('TOKEN')
-DB = connect(config('DB_URL', default='sqlite:///bot.db'))  # сама БДшка
+DB = connect(config('DATABASE_URL', default='sqlite:///bot.db'))  # сама БДшка
 PORT = config('PORT', default=8443)
 HEROKU_APP_NAME = config('HEROKU_APP_NAME', default=None)
